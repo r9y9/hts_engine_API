b6fe1ece08b15e5c73b2f8b50e968b72c8b64bcc
uratec <uratec>
2013-12-11 14:58:07 +0000

    Make android gcc happy.

--- src/lib/HTS_misc.c
+++ src/lib/HTS_misc.c
@@ -245,11 +245,11 @@ size_t HTS_ftell(HTS_File * fp)
    } else if (fp->type == HTS_FILE) {
       fpos_t pos;
       fgetpos((FILE *) fp->pointer, &pos);
-#if defined(_WIN32) || defined(__CYGWIN__) || defined(__APPLE__)
+#if defined(_WIN32) || defined(__CYGWIN__) || defined(__APPLE__) || defined(__ANDROID__)
       return (size_t) pos;
 #else
       return (size_t) pos.__pos;
-#endif                          /* _WIN32 || __APPLE__ */
+#endif                          /* _WIN32 || __CYGWIN__ || __APPLE__ || __ANDROID__ */
    }
    HTS_error(0, "HTS_ftell: Unknown file type.\n");
    return 0;
